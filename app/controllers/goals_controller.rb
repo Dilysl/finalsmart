63b2233a3551c11efefe748378699c398cf72fcc
Dilysl <dilys.lambert@gmail.com>
2015-06-08 20:25:54 +0200

    busy with coding for Goalsmodel. sum is included. can't get the description to pop up prob because it's a text and not string..

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,9 @@
 class GoalsController < ApplicationController
+
+	def show
+		@user = current_user
+		@goal = current_user.goals.find(params[:id])
+	end
 	
 	def new
 		@user = current_user
@@ -10,16 +15,25 @@ class GoalsController < ApplicationController
 		@goal = current_user.goals.build(goal_params)
 		@goal.user_id = @user.id
 		if @goal.save(goal_params)
-			redirect_to user_records_path
+			redirect_to user_goal_path
 		else
 			render :new
 		end
 	end
 
 	def edit
+		@user = current_user
+		@goal = current_user.goals.find(params[:id])
 	end
 
 	def update
+		@user = current_user
+		@goal = current_user.goals.find(params[:id])
+		if @goal.update(goal_params)
+			redirect_to user_goal_path
+		else
+			render :edit
+		end
 	end
 
 	def destroy
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,10 @@
 class RecordsController < ApplicationController
 
 	def index
+		@user = current_user
+		@records = current_user.records
+		@goals = current_user.goals
+		@sum = 0
 	end
 
 	def new
