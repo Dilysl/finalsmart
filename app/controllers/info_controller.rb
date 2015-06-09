81852ec8047750b0d00aced6cefe0eadd2010c8e
zhyman <zhyman@umich.edu>
2015-06-09 21:25:09 +0200

    FirstName LastName

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,4 +1,7 @@
 class InfoController < ApplicationController
 	def index
 	end
+
+	def show
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608193833) do
+ActiveRecord::Schema.define(version: 20150609190410) do
 
   create_table "goals", force: :cascade do |t|
     t.date     "date"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150608193833) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
