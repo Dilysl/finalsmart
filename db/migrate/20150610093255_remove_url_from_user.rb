3e41dcd732d12921ecf29353cd7ee166c8482ba7
zhyman <zhyman@umich.edu>
2015-06-10 11:43:59 +0200

    profile picture + goals

---
+++ db/migrate/20150610093255_remove_url_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveUrlFromUser < ActiveRecord::Migration
+  def change
+    remove_column :users, :url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609190410) do
+ActiveRecord::Schema.define(version: 20150610093506) do
 
   create_table "goals", force: :cascade do |t|
     t.date     "date"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20150609190410) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "profile_picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
